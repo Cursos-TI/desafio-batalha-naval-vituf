3367a896d7013167a24fde202d4e7e424c2d0d7d
vituf <vitoremanuelrs5@gmail.com>
2025-04-28 22:23:14 +0000

    coloquei os navio na diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,32 +1,56 @@
 #include <stdio.h>
 
-int main() {
-    int matriz[10][10] = {0}; // Inicializa todos os elementos com 0.
+#define linhas 10
+#define colunas 10 
+#define navio 3
 
-    int linhas;
-    int colunas;
-    
+int main() {
+    int matriz[linhas][colunas] = {0}; // Inicializa todos os elementos com 0.
+    int diagonalE = 0;
+    int diagonalD = 9;
     printf("Tabuleiro: Batalha Naval \n");
     
     // loop para o navio ocupar 3 posições horizontais.
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++)
+    for (int i = 0; i < navio; i++) {
+        for (int j = 0; j < navio; j++)
         {
-            matriz[linhas = 5][colunas + i] = 3;
+            matriz[i + 4][1] = navio;
         }
     }
 
     // loop para o navio ocupar 3 posições vertical.
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++)
+    for (int i = 0; i < navio; i++) {
+        for (int j = 0; j < navio; j++)
         {
-            matriz[linhas + j][colunas = 9] = 3;
+            matriz[4][7 + j] = navio;
         }
     }
 
-    // Mostrar a matriz no painel.
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    // loop para o navio na diagonal esquerda.
+    for (int i = 0; i < navio; i++)
+    {
+        for (int j = 0; j < navio; j++)
+        {
+            matriz[diagonalE][diagonalE] = navio;  
+        }
+        diagonalE++;
+    }
+
+    // loop para o navio na diagonal direita.
+    for (int i = 0; i < navio; i++)
+    {
+        for (int j = 0; j < navio; j++)
+        {
+            matriz[diagonalD][diagonalD] = navio;  
+        }
+        diagonalD--;
+    }
+
+    // Mostrar o tabuleiro no painel.
+    for (int i = 0; i < linhas; i++) 
+    {
+        for (int j = 0; j < colunas; j++)
+        {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
